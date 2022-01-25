9118896d11e4d049dfbdaea07b63a28001bc9dbc
Gorka <gorkavillara@gmail.com>
2022-01-25 13:03:47 +0100

    Eliminando errores de Typescript

--- pages/api/bookings/index.ts
+++ pages/api/bookings/index.ts
@@ -1,16 +1,17 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  bookings: object[];
-};
+// type Data = {
+//   bookings: object[];
+// };
 
-const Bookings = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const { action, store } = req.body;
-  if (action === "getByStore") {
-    const r = await getBookingsByStore({ id: store.id });
-    return res.status(200).json(r);
-  }
-  return res.status(404).json(req.body);
-};
+// const Bookings = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+//   const { action, store } = req.body;
+//   if (action === "getByStore") {
+//     const r = await getBookingsByStore({ id: store.id });
+//     return res.status(200).json(r);
+//   }
+//   return res.status(404).json(req.body);
+// };
 
-export default Bookings;
+// export default Bookings;
+export {}
--- pages/api/stores/index.ts
+++ pages/api/stores/index.ts
@@ -1,18 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getStoresByUserEmail } from "../../../controllers/DBController";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { getStoresByUserEmail } from "../../../controllers/DBController";
 
-type Data = {
-  stores: object[];
-  role?: string;
-};
+// type Data = {
+//   stores: object[];
+//   role?: string;
+// };
 
-const Stores = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const { action, user } = req.body;
-  if (action === "getByStore") {
-    const r = await getStoresByUserEmail({ userEmail: user.email });
-    return res.status(200).json(r);
-  }
-  return res.status(404).json(req.body);
-};
+// const Stores = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+//   const { action, user } = req.body;
+//   if (action === "getByStore") {
+//     const r = await getStoresByUserEmail({ userEmail: user.email });
+//     return res.status(200).json(r);
+//   }
+//   return res.status(404).json(req.body);
+// };
 
-export default Stores;
+// export default Stores;
+export {}
