821108b3da0287c1bfdd0a5beb691e8a45538c68
Gorka <gorkavillara@gmail.com>
2022-01-26 23:17:35 +0100

    Subida+++

--- pages/admin/components/modals/NewBooking.tsx
+++ pages/admin/components/modals/NewBooking.tsx
@@ -3,14 +3,12 @@ import { Store } from "../../../../models";
 import FormNewBooking from "./FormNewBooking";
 
 const NewBooking = ({ store }: { store: Store }) => {
-  return (
-    store && (
-      <div className="flex flex-col h-full">
-        <h1 className="font-semibold text-lg mb-6">Create New Booking</h1>
-        <FormNewBooking store={store} />
-      </div>
-    )
-  );
+  return store ? (
+    <div className="flex flex-col h-full">
+      <h1 className="font-semibold text-lg mb-6">Create New Booking</h1>
+      <FormNewBooking store={store} />
+    </div>
+  ) : null;
 };
 
 export default NewBooking;
