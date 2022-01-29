10a618573420f217b67ec05a0eaa4538abf5d4ac
Gorka <gorkavillara@gmail.com>
2022-01-29 19:02:33 +0100

    Solucionado error de cambios de vista

--- pages/admin/views/TablesView.tsx
+++ pages/admin/views/TablesView.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { AdminContext } from "..";
-import Card from "../components/Card";
 import { Booking } from "../../../models";
 import BookingsDisplay from "../components/BookingsDisplay";
 
@@ -10,48 +9,56 @@ const TablesView = () => {
   const today = new Date();
   return (
     <>
-      <div className="flex items-center justify-between p-6">
-        <h1 className="font-semibold text-lg">
-          Bookings: {today.toLocaleDateString("es-ES")}
-        </h1>
-        <div className="flex items-center gap-2">
-          <button
-            onClick={() => setFilter("all")}
-            className={`${
-              filter === "all" ? "btn-primary-blue" : "btn-secondary-blue"
-            }`}
-          >
-            All ({bookings.length})
-          </button>
-          <button
-            onClick={() => setFilter("open")}
-            className={`${
-              filter === "open" ? "btn-primary-blue" : "btn-secondary-blue"
-            }`}
-          >
-            Open (
-            {
-              bookings.filter((booking: Booking) => booking.status === "open")
-                .length
-            }
-            )
-          </button>
-          <button
-            onClick={() => setFilter("closed")}
-            className={`${
-              filter === "closed" ? "btn-primary-blue" : "btn-secondary-blue"
-            }`}
-          >
-            Closed (
-            {
-              bookings.filter((booking: Booking) => booking.status === "closed")
-                .length
-            }
-            )
-          </button>
-        </div>
-      </div>
-      <BookingsDisplay bookings={bookings} filter={filter} />
+      {bookings ? (
+        <>
+          <div className="flex items-center justify-between p-6">
+            <h1 className="font-semibold text-lg">
+              Bookings: {today.toLocaleDateString("es-ES")}
+            </h1>
+            <div className="flex items-center gap-2">
+              <button
+                onClick={() => setFilter("all")}
+                className={`${
+                  filter === "all" ? "btn-primary-blue" : "btn-secondary-blue"
+                }`}
+              >
+                All ({bookings.length})
+              </button>
+              <button
+                onClick={() => setFilter("open")}
+                className={`${
+                  filter === "open" ? "btn-primary-blue" : "btn-secondary-blue"
+                }`}
+              >
+                Open (
+                {
+                  bookings.filter(
+                    (booking: Booking) => booking.status === "open"
+                  ).length
+                }
+                )
+              </button>
+              <button
+                onClick={() => setFilter("closed")}
+                className={`${
+                  filter === "closed"
+                    ? "btn-primary-blue"
+                    : "btn-secondary-blue"
+                }`}
+              >
+                Closed (
+                {
+                  bookings.filter(
+                    (booking: Booking) => booking.status === "closed"
+                  ).length
+                }
+                )
+              </button>
+            </div>
+          </div>
+          <BookingsDisplay bookings={bookings} filter={filter} />
+        </>
+      ) : null}
     </>
   );
 };
