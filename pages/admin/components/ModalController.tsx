e99ae3237687db94af0c8c1862572bbed4a55495
Gorka <gorkavillara@gmail.com>
2022-01-31 00:03:32 +0100

    Modal con react-responsive-modal

--- pages/admin/components/ModalController.tsx
+++ pages/admin/components/ModalController.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 import AllergiesModal from "./modals/AllergiesModal";
 import DishesModal from "./modals/DishesModal";
 import NewBooking from "./modals/NewBooking";
@@ -19,27 +21,6 @@ type ModalProps = {
   data?: any;
 };
 
-const dropIn = {
-  hidden: {
-    y: "-100vh",
-    opacity: 0,
-  },
-  visible: {
-    y: "5vh",
-    opacity: 1,
-    transition: {
-      duration: 0.1,
-      type: "spring",
-      damping: 25,
-      stiffness: 500,
-    },
-  },
-  exit: {
-    y: "100vh",
-    opacity: 0,
-  },
-};
-
 const ModalController = ({
   visible,
   onClose,
@@ -47,47 +28,31 @@ const ModalController = ({
   data,
 }: ModalProps) => {
   return (
-    <AnimatePresence initial={false} exitBeforeEnter>
-      {visible ? (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          className="fixed top-0 left-0 h-full w-full bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-start z-50 overflow-scroll"
-          onClick={onClose}
-        >
-          <motion.div
-            variants={dropIn}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            className="bg-white flex justify-center items-center p-6 rounded-lg w-5/6 sm:w-3/4"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {modalContent === "newBooking" && <NewBooking store={data.store} />}
-            {modalContent === "allergies" && (
-              <AllergiesModal allergies={data} />
-            )}
-            {modalContent === "notes" && (
-              <NotesModal
-                notes={data.booking.notes}
-                booking={data.booking}
-                closeModal={onClose}
-              />
-            )}
-            {modalContent === "tables" && <TablesModal />}
-            {modalContent === "tables-multiple" && <TablesModalMultiple />}
-            {modalContent === "newDish" && <DishesModal />}
-            {modalContent === "editDish" && <DishesModal editDish={data} />}
-            {modalContent === "newMenu" && <MenusModal />}
-            {modalContent === "editMenu" && <MenusModal editMenu={data} />}
-            {modalContent === "closeBooking" && <CloseBooking booking={data} />}
-            {modalContent === "openBooking" && <OpenBooking booking={data} />}
-            <button onClick={onClose}><HiOutlinePlus className="rotate-45 text-slate-700 text-2xl font-bold absolute top-5 right-5" /></button>
-          </motion.div>
-        </motion.div>
-      ) : null}
-    </AnimatePresence>
+    <Modal
+      showCloseIcon={false}
+      open={visible}
+      center
+      closeOnOverlayClick={true}
+      onClose={onClose}
+    >
+      {modalContent === "newBooking" && <NewBooking store={data.store} />}
+      {modalContent === "allergies" && <AllergiesModal allergies={data} />}
+      {modalContent === "notes" && (
+        <NotesModal
+          notes={data.booking.notes}
+          booking={data.booking}
+          closeModal={onClose}
+        />
+      )}
+      {modalContent === "tables" && <TablesModal />}
+      {modalContent === "tables-multiple" && <TablesModalMultiple />}
+      {modalContent === "newDish" && <DishesModal />}
+      {modalContent === "editDish" && <DishesModal editDish={data} />}
+      {modalContent === "newMenu" && <MenusModal />}
+      {modalContent === "editMenu" && <MenusModal editMenu={data} />}
+      {modalContent === "closeBooking" && <CloseBooking booking={data} />}
+      {modalContent === "openBooking" && <OpenBooking booking={data} />}
+    </Modal>
   );
 };
 
